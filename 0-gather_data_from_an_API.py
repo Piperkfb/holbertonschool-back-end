d49d30bb077f05bbdf87d201fa99fc04f874bf55
Piperkfb <piperkfb@gmail.com>
2023-07-26 08:04:56 -0700

    push :(

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""USe the REST API"""
+
+import requests
+from sys import argv
+
+
+"employee :NAME: us done with tasks(:number of done:/Total number of tasks)"
+"list tasks done"
